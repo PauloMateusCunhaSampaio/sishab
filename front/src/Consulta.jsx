e96061f03182d9785825cc4023a2df6152d52399
Tales <tales.silva.leo@gmail.com>
2023-06-23 18:39:43 -0300

    versao bugada inicial

---
+++ front/src/Consulta.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Consulta() {
+  return (
+    <p>Consulta</p>
+  )
+}
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { Children } from 'react' 
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './Home'
+import Consulta from './Consulta'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/consulta", element: <Consulta /> },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+
